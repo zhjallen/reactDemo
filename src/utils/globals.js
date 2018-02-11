2f1979979d1e61590abd45cda8be8d4961a7a6aa
zhaohj <1098414377@qq.com>
2018-02-11 22:44:54 +0800

    初始化

---
+++ src/utils/globals.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, IndexRedirect, Link, browserHistory } from 'react-router';
+/**
+ * 全局变量
+ */
+window.React = React;
+window.ReactDOM = ReactDOM;
+/**
+ * 路由
+ */
+window.Link = Link;
+window.Router = Router;
+window.Route = Route;
+window.IndexRoute = IndexRoute;
+window.RootRoute = RootRoute;
+window.ModuleRoute = ModuleRoute;
+window.IndexRedirect = IndexRedirect;
+window.History = browserHistory;
+
